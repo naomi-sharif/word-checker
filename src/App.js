985bfd309d090e7a7a716a91c2515cbc4a38aab1
Naomi <hello@naomi.dev>
2019-10-22 13:27:48 +0100

    Update page title if it is a palendrome"

--- src/App.js
+++ src/App.js
@@ -5,17 +5,22 @@ export const App = () => {
   const [palindrome, setPalindrome] = useState("");
 
   const myFunction = () => {
+   if(typeof input !== 'string' || input.length > 30) {
+     return;
+   }
     const inputToLowerCase = input.toLowerCase();
     console.log(inputToLowerCase);
+
     const inputReversed = inputToLowerCase.split("").reverse().join("");
     console.log(inputReversed);
+
     const isPalindrome = inputReversed === inputToLowerCase;
     setPalindrome(isPalindrome);
   }
 
   return (
   <form>
-    <h1>Word Checker App</h1>
+    {palindrome ? <h1>{input}</h1> : <h1>Word Checker</h1>}
     <input type="text" onChange={e => setInput(e.target.value)} />
     <button type="button" onClick={myFunction}>Subtmit</button>
     {palindrome ? <p>This is a palindrome</p> : <p> Sorry! This is not a palindrome</p> }
