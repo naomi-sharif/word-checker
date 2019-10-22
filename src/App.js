3a565a9db829709dc96f668992d24f434859d2fc
Naomi <hello@naomi.dev>
2019-10-22 12:01:04 +0100

    Added check to see if word is palindrome

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@ import React, { useState}  from 'react';
 
 export const App = () => {
   const [input, setInput] = useState("");
+  const [palindrome, setPalindrome] = useState("");
 
   const myFunction = () => {
     const inputToLowerCase = input.toLowerCase();
     console.log(inputToLowerCase);
     const inputReversed = inputToLowerCase.split("").reverse().join("");
     console.log(inputReversed);
-    if(inputToLowerCase === inputReversed) {
-      console.log('Yes');
-    } else {
-      console.log('No');
-    }
+    const isPalindrome = inputReversed === inputToLowerCase;
+    setPalindrome(isPalindrome);
   }
 
   return (
@@ -20,6 +18,7 @@ export const App = () => {
     <h1>Word Checker App</h1>
     <input type="text" onChange={e => setInput(e.target.value)} />
     <button type="button" onClick={myFunction}>Subtmit</button>
+    {palindrome ? <p>This is a palindrome</p> : <p> Sorry! This is not a palindrome</p> }
   </form>
   );
 }
